24614738d74f492d67bd8f7b4a046358b8021ccf
a.dalfarra <a.dalfarra@vargroup.it>
2021-09-19 20:06:25 +0200

    piu caldo

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,5 +1,5 @@
 fbsetbg -a -c -r /home/andrea/doc/screen/ 
-redshift  -l 44.4335430:8.8540500 -t 6000:4000 &
+redshift  -l 44.4335430:8.8540500 -t 6000:3000 &
 #/home/andrea/bin/davmail/davmail.sh &
 compton & 
 gkrellm -w 
