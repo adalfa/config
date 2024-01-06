655c882a32abdd99f5b186929791f0ce73c1fcd6
a.dalfarra <a.dalfarra@vargroup.it>
2024-01-06 07:20:13 +0100

    dunst

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,5 +1,8 @@
 fbsetbg -a -c -r /home/andrea/doc/screen/ 
 redshift  -l 44.4335430:8.8540500 -t 6000:3000 &
+input-leaps   --disable-crypto
+dunst &
+
 #/home/andrea/bin/davmail/davmail.sh &
 compton & 
 gkrellm -w 
