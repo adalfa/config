41a707fd92df97baf31c236641f50f5497268ae8
a.dalfarra <a.dalfarra@vargroup.it>
2025-01-10 18:56:16 +0100

    inputleaps-

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,6 +1,6 @@
 fbsetbg -a -c -r /home/andrea/doc/screen/ 
 redshift  -l 44.4335430:8.8540500 -t 6000:3500 &
-input-leaps   --disable-crypto
+#input-leaps   --disable-crypto
 setxkbmap -device `xinput list --id-only "Virtual core XTEST keyboard"` it ibm
 
 dunst &
